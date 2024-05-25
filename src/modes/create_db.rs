1f14f2270de637d618ec03f67638ac4f4f032456
s.aran <s-aran@users.noreply.github.com>
2024-05-25 19:46:17 +0900

    fix os error when no db file with --init-db option

--- src/modes/create_db.rs
+++ src/modes/create_db.rs
@@ -16,6 +16,7 @@ pub struct CreateDatabaseMode {}
 impl Mode for CreateDatabaseMode {
     fn run(&self) -> ExitCode {
         let db_path = Path::new(HASH_TABLE_FILENAME);
+        if db_path.exists() {
         match std::fs::remove_file(&db_path) {
             Ok(_) => {}
             Err(e) => {
@@ -23,8 +24,7 @@ impl Mode for CreateDatabaseMode {
                 return 1.into();
             }
         };
-
-        let mut connection = Connection::open(&db_path).unwrap();
+        }
 
         let mut file_list: Vec<PathBuf> = vec![];
         glob_with_recursive("./*", &mut |p| {
